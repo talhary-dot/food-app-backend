c0a9f1f34afcf6af376544957daf71e992e2160f
Naveed122-ux <Naveed@woltrio.com>
2025-03-10 13:52:55 +0500

    updated

--- utils/calculateDiscount.js
+++ utils/calculateDiscount.js
@@ -1,8 +1,10 @@
-export function calculatePrice(price, discount, quantity, discountTime) {
+function calculatePrice(price, discount, quantity, discountTime) {
   if (new Date() > new Date(discountTime)) {
     return price * quantity;
   } else {
-    const discountedPrice = price - (price * discount) / 100; // Calculate price after discount
-    return discountedPrice * quantity; // Return the total price after discount
+    const discountedPrice = price - (price * discount) / 100;
+    return discountedPrice * quantity;
   }
 }
+
+module.exports = calculatePrice;
